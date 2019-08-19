f9a5257d5f53603f2850e0feed3525c3d71ae0bb
Maxence MOHR <contact@fladnag.net>
2019-08-19 16:17:24 +0200

    Added base

---
+++ PKCS11Test/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Markup.Xaml;
+
+namespace PKCS11Test
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
