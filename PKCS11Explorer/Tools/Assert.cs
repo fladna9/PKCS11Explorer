a6de7aa60bafe7bd4223dcbe1b65fb2701686b5e
Maxence MOHR <contact@fladnag.net>
2019-08-21 15:16:13 +0200

    Small assert to make tests

---
+++ PKCS11Explorer/Tools/Assert.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PKCS11Explorer.Tools
+{
+    static class Assert
+    {
+        public static void AssertTrue(bool thingToAssert, string tag = null)
+        {
+            if (!thingToAssert)
+                throw new AssertException("[" + tag + "] AssertTrue: is false, expected true");
+        }
+
+        public static void AssertFalse(bool thingToAssert, string tag = "Assert")
+        {
+            if (thingToAssert)
+                throw new AssertException("[" + tag + "] AssertFalse: is true, expected false");
+        }
+
+        public static void AssertICollectionEmptyOrNull(ICollection thingToAssert, string tag = "Assert")
+        {
+            if (thingToAssert != null && thingToAssert.Count > 0)
+                throw new AssertException("[" + tag + "] AssertICollectionEmptyOrNull: is populated, expected null or empty");
+        }
+
+        public static void AssertICollectionPopulated(ICollection thingToAssert, string tag = "Assert")
+        {
+            if (thingToAssert != null && thingToAssert.Count > 0)
+                throw new AssertException("[" + tag + "] AssertICollectionPopulated: is null or empty, expected populated");
+        }
+    }
+
+    public class AssertException : Exception
+    {
+        public AssertException(string message) : base(message) {}
+    }
+}
